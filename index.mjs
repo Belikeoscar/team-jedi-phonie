8bc277476296ee8b16888762464b745cbdc7d683
Scar <61349794+scarnation@users.noreply.github.com>
2022-07-24 19:53:01 +0000

    mod on aside section

--- index.mjs
+++ index.mjs
@@ -1,17 +1,19 @@
 import startApp from './app.mjs';
 
-document.addEventListener('DOMContentLoaded', startApp); 
+document.addEventListener('DOMContentLoaded', startApp);
 
 let bar = document.querySelector('.bx-menu')
 let exit = document.querySelector('.bx-x')
 let menu = document.querySelector('ul')
 
-bar.addEventListener('click', ()=>{
-   
-    menu.style.transform='translateX(0%)'
-    
+bar.addEventListener('click', () => {
+
+  menu.style.transform = 'translateX(0%)'
+
 })
 
-exit.addEventListener('click', ()=>{
-   menu.style.transform='translateX(100%)'
+exit.addEventListener('click', () => {
+  menu.style.transform = 'translateX(100%)'
 })
+
+
