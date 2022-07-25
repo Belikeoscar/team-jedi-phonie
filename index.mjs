c11eb984078c10b1e426fe0036028264f5f488cf
Victory Okirie <78309631+victoryokirie@users.noreply.github.com>
2022-07-25 19:16:41 +0000

    styling input fields for when it is filled

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import startApp from './app.mjs';
 
 document.addEventListener('DOMContentLoaded', startApp);
 
+//opening and closing the side navbar on smaller screen sizes
 let bar = document.querySelector('.bx-menu')
 let exit = document.querySelector('.bx-x')
 let menu = document.querySelector('ul')
@@ -14,6 +15,21 @@ bar.addEventListener('click', () => {
 
 exit.addEventListener('click', () => {
   menu.style.transform = 'translateX(100%)'
+})
+
+//adding color to the input field once it is filled 
+let inputField = document.querySelectorAll('input')
+
+inputField.forEach(input => {
+  input.addEventListener('keyup', (e) => {
+
+    if (input.value !== "") {
+      input.style.backgroundColor = '#FAF5F3'
+    }
+    else {
+      input.style.backgroundColor = '#fff'
+    }
+  })
 })
 
 
